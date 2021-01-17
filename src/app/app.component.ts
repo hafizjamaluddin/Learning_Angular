89a769a25f21f873022f3e993722b2b8ed45916d
hafizjamaluddin <hafizjamaluddin83@gmail.com>
2021-01-17 15:55:36 +0800

    3 files modified, 1 added (template driven form)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,22 @@
-import { Component, VERSION } from '@angular/core';
-
+import { Component, VERSION } from "@angular/core";
+import { FormGroup, FormControl } from "@angular/forms";
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  title = 'Angular 6 Projects !';
-  todaydate = new Date();
-  jsonval = {name:'Rox', age:'25', address:{a1:'Mumbai', a2:'Karnataka'}};
-  months = ["January", "Feburary", "March", "April", "May", 
-            "June", "July", "August", "September",
-            "October", "November", "December"];
-  
-   isavailable = true;
-   myClickFunction(event) {
-      this.isavailable = false;
-   }
-      changemonths(event) {
-      alert("Changed month from the Dropdown");
-   }
+export class AppComponent {
+  title = "Angular 6 Projects !";
+  todaydate;
+  componentproperty;
+  fname;
+  lname;
+  formdata;
+  email;
+
+  onClickSubmit(data) {
+    alert(
+      "Entered Full Name : " +data.fname +" " +data.lname +"\nEntered Email : " +data.email
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { MyserviceService } from './myservice.service';
+import { HttpModule } from '@angular/http';
 import { SqrtPipe } from './app.sqrt';
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,  ],
-  declarations: [ AppComponent, HelloComponent, SqrtPipe ],
+  imports:      [ BrowserModule, FormsModule,       HttpModule,  ],
+  declarations: [ AppComponent, HelloComponent, SqrtPipe,        ],
+  providers: [MyserviceService],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
